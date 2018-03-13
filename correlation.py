efd6f856fa7226933d1ecde8c530faf27ff8b8a5
rw1993 <470177412@qq.com>
2018-03-13 10:23:47 +0800

    read data and calculate correlation

--- correlation.py
+++ correlation.py
@@ -1,6 +1,22 @@
 # -*- coding: utf-8 -*-
-from read_data import lines, get_feature_from_line
-
+from read_data import normalize_data
+from scipy.stats import pearsonr
+lenth, dimensions = normalize_data.shape
 # get_data
-import ipdb; ipdb.set_trace()
+
+def get_pearson():
+    pearson_dict = {}
+    for i in range(dimensions):
+        for j in range(i+1, dimensions):
+            r = pearsonr(normalize_data[:,i], normalize_data[:,j])
+            pearson_dict[(i, j)] = pearson_dict[(j, i)] = abs(r[0]) 
+    return pearson_dict
+pearson_dict = get_pearson()
+
+
+if __name__ == "__main__":
+    get_pearson()
+
+
+    
 
--- read_data.py
+++ read_data.py
@@ -1,20 +1,29 @@
 # -*- coding: utf-8 -*-
 from sklearn.preprocessing import Normalizer
 import numpy as np
+
 with open("yfj.csv", "r") as f:
     lines = f.readlines()[2:]
 
+counter = 1
 def get_feature_from_line(line):
+    global counter
     attributes = line.strip().split(",")
-    feature = map(float, attributes[1:])
-    return np.array(feature)
+    try:
+        feature = list(map(float, attributes[1:]))
+        if len(feature) != 40:
+            return None
+    except:
+        return None
+    print counter
+    counter += 1
+    return feature
 
 
-numpy_data = map(get_feature_from_line, lines)
+numpy_data = list(filter(lambda x: x is not None,
+                  map(get_feature_from_line, lines)))
+
 numpy_data = np.array(numpy_data)
-import ipdb; ipdb.set_trace()
 normalizer = Normalizer()
 normalizer.fit(numpy_data)
 normalize_data = normalizer.transform(numpy_data)
-
-import ipdb; ipdb.set_trace()
