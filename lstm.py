dc05d96c50bf6fe34aa0e3d0dead0bc22e727cda
rw1993 <470177412@qq.com>
2018-03-15 18:14:06 +0800

    auto reducing learning rate

--- lstm.py
+++ lstm.py
@@ -72,15 +72,17 @@ def main():
     Q = crf_layer(net, batch_size, dimensions, [pearson_dict])
     flag = "train"
     if flag == "train":
-        label_tensor = tf.placeholder(dtype=np.int32, shape=(batch_size, dimensions))
+        learning_rate = 0.001
+        label_tensor = tf.placeholder(dtype=tf.int32, shape=(batch_size, dimensions))
         loss_tensor = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(logits=Q,
                                                                             labels=tf.one_hot(label_tensor,
                                                                                               2)))
         loss_tensor = loss_tensor / batch_size
+        learning_rate_tensor = tf.placeholder(dtype=tf.float32, shape=())
                                                                              
 
         # train_step = tf.train.AdamOptimizer(1e-8).minimize(loss_tensor)
-        optimizer = tf.train.AdamOptimizer(1e-3)
+        optimizer = tf.train.AdamOptimizer(learning_rate_tensor)
         gradients = optimizer.compute_gradients(loss_tensor)
         cilp_gradients = [(tf.clip_by_value(g, -5.0, 5.0), v) for g, v in gradients]
         train_step = optimizer.apply_gradients(cilp_gradients)
@@ -93,12 +95,15 @@ def main():
             sess.run(init)
             recent_avgloss = 0.0
             saver = tf.train.Saver()
+            former_acc = 0.0
             print "builded"
             for bx, by, bf in generate_batch():
                 _, summary_string, loss = sess.run([train_step, merge_summary_op, loss_tensor],
                                                    feed_dict={input_tensor: bx,
-                                                        dropout_tensor: 0.8,
-                                                        label_tensor: by})
+                                                   dropout_tensor: 0.8,
+                                                   learning_rate_tensor:
+                                                   learning_rate,
+                                                   label_tensor: by})
                 summary_writer.add_summary(summary_string, step)
                 step += 1
                 # print loss, step
@@ -109,13 +114,14 @@ def main():
                 if step % 10000 == 0:
                     saver.save(sess, "lstm_model/", global_step=step)
                     # valid
-                    valid_num = 5000
+                    valid_num = 10000
                     total = 0
                     acc = 0.0
                     valid = 0
                     for bx, by, bf in generate_batch('test'):
                         q_result, = sess.run([Q,], feed_dict={input_tensor: bx,
                                                    dropout_tensor: 1.0,
+                                                   learning_rate_tensor: learning_rate,
                                                    label_tensor: by})
                         valid += 1
                         for b in range(batch_size):
@@ -129,6 +135,9 @@ def main():
                                         acc += 1
                         if valid >= valid_num:
                             break
+                    if float(acc) / total < former_acc:
+                        learning_rate /= 10.0
+                    former_acc = float(acc) / total
                     print "valid acc is {} after step {}".format(float(acc)/total, step)
 
 
